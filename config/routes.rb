fc210d4f246225c51a33fa581f767de16a0add6f
jean <nguyen.jean@me.com>
2018-09-10 09:49:51 +0200

    change header, change footer, add /profil page, add form compte & s'inscrire

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  resources :contacts
+  resources :services
+
+  # devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+
   root 'home#index'
   get 'about', to: 'home#about'
-  get 'contact', to: 'home#contact'
-  resources :services
+  get 'contacts', to: 'contacts#index'
+  get 'profil', to: 'home#profil'
   devise_for :admins
   devise_for :users
+
+  get '*path' => redirect { '/' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
